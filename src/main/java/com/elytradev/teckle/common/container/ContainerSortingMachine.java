58e63dfde01369408cc9341b55b0de83f8867c7d
darkevilmac <bk1325@gmail.com>
2017-06-04 20:18:52 -0700

    Fix shift clock in sorting machine.

--- src/main/java/com/elytradev/teckle/common/container/ContainerSortingMachine.java
+++ src/main/java/com/elytradev/teckle/common/container/ContainerSortingMachine.java
@@ -22,6 +22,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.SlotItemHandler;
 
 /**
@@ -62,6 +63,39 @@ public class ContainerSortingMachine extends Container {
         for (int i = 0; i < 9; i++) {
             addSlotToContainer(new Slot(inventoryplayer, i, 8 + i * 18, 76 + 142));
         }
+    }
+
+
+    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
+        ItemStack itemstack = ItemStack.EMPTY;
+        Slot slot = this.inventorySlots.get(index);
+
+        if (slot != null && slot.getHasStack()) {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+
+            if (index < 48) {
+                if (!this.mergeItemStack(itemstack1, 48, 84, true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!this.mergeItemStack(itemstack1, 0, 48, false)) {
+                return ItemStack.EMPTY;
+            }
+
+            if (itemstack1.isEmpty()) {
+                slot.putStack(ItemStack.EMPTY);
+            } else {
+                slot.onSlotChanged();
+            }
+
+            if (itemstack1.getCount() == itemstack.getCount()) {
+                return ItemStack.EMPTY;
+            }
+
+            slot.onTake(playerIn, itemstack1);
+        }
+
+        return itemstack;
     }
 
     /**
