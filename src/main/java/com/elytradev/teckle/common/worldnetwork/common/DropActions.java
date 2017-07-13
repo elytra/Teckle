ca54daaeb74767ccf20eb1939346cd99cb71e227
darkevilmac <bk1325@gmail.com>
2017-07-12 22:02:05 -0700

    Add a metric ton of debug info to DropActions in an attempt to fix a bug.

--- src/main/java/com/elytradev/teckle/common/worldnetwork/common/DropActions.java
+++ src/main/java/com/elytradev/teckle/common/worldnetwork/common/DropActions.java
@@ -16,6 +16,7 @@
 
 package com.elytradev.teckle.common.worldnetwork.common;
 
+import com.elytradev.teckle.common.TeckleMod;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -25,18 +26,26 @@ import net.minecraft.world.World;
 
 import java.util.HashMap;
 
-/**
- * Created by darkevilmac on 4/10/2017.
- */
 public class DropActions {
     public static final HashMap<String, IDropAction> ACTIONS = new HashMap<>();
 
     public static Tuple<String, IDropAction> ITEMSTACK = new Tuple<>("itemstack", traveller -> {
-        World world = traveller.network.getWorld();
-        BlockPos nodePos = traveller.currentNode.position;
-        world.spawnEntity(new EntityItem(world, nodePos.getX(), nodePos.getY(),
-                nodePos.getZ(), new ItemStack(traveller.data.getCompoundTag("stack"))));
-        traveller.data.setTag("stack", new NBTTagCompound());
+        try {
+            World world = traveller.network.getWorld();
+            BlockPos nodePos = traveller.currentNode.position;
+            world.spawnEntity(new EntityItem(world, nodePos.getX(), nodePos.getY(),
+                    nodePos.getZ(), new ItemStack(traveller.data.getCompoundTag("stack"))));
+            traveller.data.setTag("stack", new NBTTagCompound());
+        } catch (NullPointerException npe) {
+            String debugInfo = "Traveller " + traveller == null ? "null" : traveller.toString();
+            debugInfo += " network " + traveller.network == null ? "null" : traveller.network.toString();
+            debugInfo += " world " + traveller.network.getWorld() == null ? "null" : traveller.network.getWorld().toString();
+            TeckleMod.LOG.error("****************OH SHIT TECKLE BROKE*******************");
+            TeckleMod.LOG.error("Caught NPE in DropActions, {}", traveller);
+            TeckleMod.LOG.error("Exception follows, {}", npe);
+            TeckleMod.LOG.error("Here's some useful debug info, {}", debugInfo);
+            TeckleMod.LOG.error("****************OH SHIT TECKLE BROKE*******************");
+        }
     });
 
     static {
