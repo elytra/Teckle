50d88049a20cf0578274702cfd2b7691fb6ddb71
darkevilmac <bk1325@gmail.com>
2017-08-04 01:43:48 -0700

    Fix no shadows in guis because mojang is not smart sometimes.

--- src/main/java/com/elytradev/teckle/client/gui/GuiFilter.java
+++ src/main/java/com/elytradev/teckle/client/gui/GuiFilter.java
@@ -53,6 +53,13 @@ public class GuiFilter extends GuiContainer {
     }
 
     @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+        this.renderHoveredToolTip(mouseX, mouseY);
+    }
+
+    @Override
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
         super.drawGuiContainerForegroundLayer(mouseX, mouseY);
         fontRenderer.drawString(I18n.format("gui.inventory"), 8, 73, 0x404040);
