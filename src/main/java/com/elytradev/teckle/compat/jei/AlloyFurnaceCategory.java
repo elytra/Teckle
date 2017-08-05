9d08c72fc3dec8653488e16183d31b6391b67464
darkevilmac <bk1325@gmail.com>
2017-08-04 22:50:59 -0700

    Add alloy recipes to JEI, need to fix transforms and rendering as its just a mix of the crafting grid and furnace implementation from JEI's Vanilla plugin.

---
+++ src/main/java/com/elytradev/teckle/compat/jei/AlloyFurnaceCategory.java
@@ -0,0 +1,102 @@
+package com.elytradev.teckle.compat.jei;
+
+import com.elytradev.teckle.common.TeckleMod;
+import mezz.jei.api.IGuiHelper;
+import mezz.jei.api.gui.*;
+import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.IRecipeCategory;
+import mezz.jei.api.recipe.IRecipeWrapper;
+import mezz.jei.api.recipe.wrapper.ICustomCraftingRecipeWrapper;
+import mezz.jei.api.recipe.wrapper.IShapedCraftingRecipeWrapper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
+import java.util.List;
+
+public class AlloyFurnaceCategory implements IRecipeCategory<IRecipeWrapper> {
+
+    public static final int width = 116;
+    public static final int height = 54;
+    private static final int craftOutputSlot = 0;
+    private static final int craftInputSlot1 = 1;
+    private final IDrawable background;
+
+    private final String localizedName;
+    private final ICraftingGridHelper craftingGridHelper;
+    private final IDrawableAnimated flame;
+    private final IDrawableAnimated arrow;
+
+    public AlloyFurnaceCategory(IGuiHelper guiHelper) {
+        ResourceLocation location = new ResourceLocation("teckle", "textures/gui/alloyfurnace.png");
+        background = guiHelper.createDrawable(location, 29, 16, width, height);
+        localizedName = I18n.format("tile.teckle.alloyfurnace.name");
+        craftingGridHelper = guiHelper.createCraftingGridHelper(craftInputSlot1, craftOutputSlot);
+
+        IDrawableStatic flameDrawable = guiHelper.createDrawable(location, 176, 0, 14, 14);
+        flame = guiHelper.createAnimatedDrawable(flameDrawable, 300, IDrawableAnimated.StartDirection.TOP, true);
+
+        IDrawableStatic arrowDrawable = guiHelper.createDrawable(location, 176, 14, 24, 17);
+        arrow = guiHelper.createAnimatedDrawable(arrowDrawable, 200, IDrawableAnimated.StartDirection.LEFT, false);
+    }
+
+    @Override
+    public String getUid() {
+        return TeckleJEIPlugin.ALLOYRECIPE_UID;
+    }
+
+    @Override
+    public String getTitle() {
+        return localizedName;
+    }
+
+    @Override
+    public String getModName() {
+        return TeckleMod.MOD_NAME;
+    }
+
+    @Override
+    public IDrawable getBackground() {
+        return background;
+    }
+
+    @Override
+    public void drawExtras(Minecraft minecraft) {
+        flame.draw(minecraft, 2, 20);
+        arrow.draw(minecraft, 24, 18);
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayout recipeLayout, IRecipeWrapper recipeWrapper, IIngredients ingredients) {
+        IGuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
+
+        guiItemStacks.init(craftOutputSlot, false, 94, 18);
+
+        for (int y = 0; y < 3; ++y) {
+            for (int x = 0; x < 3; ++x) {
+                int index = craftInputSlot1 + x + (y * 3);
+                guiItemStacks.init(index, true, x * 18, y * 18);
+            }
+        }
+
+        if (recipeWrapper instanceof ICustomCraftingRecipeWrapper) {
+            ICustomCraftingRecipeWrapper customWrapper = (ICustomCraftingRecipeWrapper) recipeWrapper;
+            customWrapper.setRecipe(recipeLayout, ingredients);
+            return;
+        }
+
+        List<List<ItemStack>> inputs = ingredients.getInputs(ItemStack.class);
+        List<List<ItemStack>> outputs = ingredients.getOutputs(ItemStack.class);
+
+        if (recipeWrapper instanceof IShapedCraftingRecipeWrapper) {
+            IShapedCraftingRecipeWrapper wrapper = (IShapedCraftingRecipeWrapper) recipeWrapper;
+            craftingGridHelper.setInputs(guiItemStacks, inputs, wrapper.getWidth(), wrapper.getHeight());
+        } else {
+            craftingGridHelper.setInputs(guiItemStacks, inputs);
+            recipeLayout.setShapeless();
+        }
+        guiItemStacks.set(craftOutputSlot, outputs.get(0));
+    }
+
+}
