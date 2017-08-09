286bdd2fd5f5beb8f6754c266c5fdb050ad19f1d
darkevilmac <bk1325@gmail.com>
2017-08-09 01:56:32 -0700

    Add exception for missing ores instead of just existing the game.

---
+++ src/main/java/com/elytradev/teckle/common/exception/MissingOreException.java
@@ -0,0 +1,7 @@
+package com.elytradev.teckle.common.exception;
+
+public class MissingOreException extends RuntimeException {
+    public MissingOreException(String data) {
+        super(data);
+    }
+}
