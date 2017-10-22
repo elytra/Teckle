bb1f986361722a675a6c75d043004f85612ab26c
darkevilmac <bk1325@gmail.com>
2017-10-22 14:48:17 -0700

    Add container slots to retriever.

--- src/main/java/com/elytradev/teckle/common/container/ContainerRetriever.java
+++ src/main/java/com/elytradev/teckle/common/container/ContainerRetriever.java
@@ -2,7 +2,11 @@ package com.elytradev.teckle.common.container;
 
 import com.elytradev.teckle.common.tile.retriever.TileRetriever;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.items.SlotItemHandler;
 
 public class ContainerRetriever extends Container {
 
@@ -12,10 +16,60 @@ public class ContainerRetriever extends Container {
     public ContainerRetriever(TileRetriever retriever, EntityPlayer player) {
         this.retriever = retriever;
         this.player = player;
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                this.addSlotToContainer(new SlotItemHandler(retriever.filterData.getHandler(), j + i * 3, 62 + j * 18, 17 + i * 18));
+            }
+        }
+        bindPlayerInventory(player.inventory);
     }
 
+    protected void bindPlayerInventory(InventoryPlayer inventoryplayer) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 9; j++) {
+                addSlotToContainer(new Slot(inventoryplayer, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            addSlotToContainer(new Slot(inventoryplayer, i, 8 + i * 18, 142));
+        }
+    }
     @Override
     public boolean canInteractWith(EntityPlayer player) {
         return retriever.isUsableByPlayer(player);
+    }
+
+    @Override
+    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
+        ItemStack stackSlotCopy = ItemStack.EMPTY;
+        Slot slot = this.inventorySlots.get(index);
+
+        if (slot != null && slot.getHasStack()) {
+            ItemStack stackInSlot = slot.getStack();
+            stackSlotCopy = stackInSlot.copy();
+
+            if (index < 9) {
+                if (!this.mergeItemStack(stackInSlot, 9, 45, true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!this.mergeItemStack(stackInSlot, 0, 9, false)) {
+                return ItemStack.EMPTY;
+            }
+
+            if (stackInSlot.isEmpty()) {
+                slot.putStack(ItemStack.EMPTY);
+            } else {
+                slot.onSlotChanged();
+            }
+
+            if (stackInSlot.getCount() == stackSlotCopy.getCount()) {
+                return ItemStack.EMPTY;
+            }
+
+            slot.onTake(playerIn, stackInSlot);
+        }
+
+        return stackSlotCopy;
     }
 }
