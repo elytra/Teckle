9d6f91f1e261b64ffc1708b46f1ff48fcac919d8
TheCodedOne <coded@hrznstudio.com>
2018-07-31 11:25:36 +0100

    Remove accidental file

--- src/main/java/com/elytradev/teckle/common/block/package-info.java
+++
@@ -1,7 +0,0 @@
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-package com.elytradev.teckle.common.block;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
